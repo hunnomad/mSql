8ed5fd241cea5fd266337076724ca0377cb19664
hunnomad <hunnomad@gmail.com>
2014-10-27 12:34:00 +0100

    small changes
    Change table name

--- examples.php
+++ examples.php
@@ -9,23 +9,23 @@ $sql = new SQL("localhost","root",'','test','en_US');
 
 // Insert record
 $iData = array('nev'=>'Böszörményi Imre Zsolt','szulEv'=>'1969.04.08','szulHely'=>'Gyula','anyjaNeve'=>'Seres Ilona');
-echo $sql->insert('probaTabla',$iData);
+echo $sql->insert('testTable',$iData);
 
 // Update data
 $iDataUpdate = array('nev'=>'Imre Zsolt Boszormenyi','szulEv'=>'1969.04.08','szulHely'=>'Gyula','anyjaNeve'=>'Seres Ilona');
-echo $sql->update("probaTabla",$iDataUpdate,"id='1'");
+echo $sql->update("testTable",$iDataUpdate,"id='1'");
 
 //Delete record
-echo $sql->delete("probaTabla","id='1'");
+echo $sql->delete("testTable","id='1'");
 
 // Select data
 $fieldArray = array('nev','szulEv','szulHely','anyjaNeve');
-$return = $sql->select("probaTabla",$fieldArray,"nev like 'Zsolt%'",'nev ASC',0);
+$return = $sql->select("testTable",$fieldArray,"nev like 'Zsolt%'",'nev ASC',0);
 print_r($return);
 
 // Select one record
 $fieldArray = array('nev','szulEv','szulHely','anyjaNeve');
-$return = $sql->selectOne("probaTabla",$fieldArray,"nev like 'Zsolt%'",'nev ASC');
+$return = $sql->selectOne("testTable",$fieldArray,"nev like 'Zsolt%'",'nev ASC');
 print_r($return);
 
 ?>
